d5a0b0ea2baf13aa0af6bb9a3d8f222356815414
Christian Tirado <tiradoc02@gmail.com>
2019-05-20 20:12:59 -0400

    initial commit :)

---
+++ views/calcGPA/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Calculate GPA';
+$this->params['breadcrumbs'][] = $this->title;
+
+
---
+++ views/site/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+
+
+$this->title = 'My Yii Application';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">You have successfully created your Yii-powered application.</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+        <div class="row">
+         <h1>sfjidsjfs9d</h1>
+    </div>
+</div>
